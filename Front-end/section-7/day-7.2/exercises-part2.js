3a663d12ac9558b9af1ad778eaff8d84effea000
isabellavjs <isabellavjs@gmail.com>
2020-08-12 13:11:55 -0300

    Corrections exercise 7.2

--- Front-end/section-7/day-7.2/exercises-part2.js
+++ Front-end/section-7/day-7.2/exercises-part2.js
@@ -29,7 +29,7 @@ console.log(addMorning(lesson2,'turno','manha'));
 //Task 2: list the keys of an object
 
 const listKeys = (object) => {
-  let arrayKeys = Object.keys(object);
+  const arrayKeys = Object.keys(object);
   return arrayKeys;
 }
 
@@ -37,14 +37,14 @@ console.log(listKeys(lesson1));
 
 //Task 3: show the size of an object
 const objectSize = (object) => {
-  let numberOfKeys = Object.keys(object);
+  const numberOfKeys = Object.keys(object);
   return `The object has ${numberOfKeys.length} properties`
 }
 console.log(objectSize(lesson3));
 
 //Task 4: List all values of an object
 const objectValues = (object) => {
-  let arrayValues = Object.values(object);
+  const arrayValues = Object.values(object);
   return arrayValues;
 }
 console.log(objectValues(lesson3));
@@ -57,7 +57,7 @@ console.log(allLessons);
 //Task 6: Return the total number in the classes
 const numberOfStudents = (object) => {
   let totalStudents = 0;
-  let allValues = Object.values(object);
+  const allValues = Object.values(object);
   for (let index in allValues) {
     totalStudents += allValues[index].numeroEstudantes;
   }
@@ -67,7 +67,7 @@ console.log(numberOfStudents(allLessons));
 
 //Task 7: Return the key value to a certain position
 const valuePosition = (object,position) => {
-  let valuesObject = Object.values(object);
+  const valuesObject = Object.values(object);
   return valuesObject[position]; 
 }
 console.log(valuePosition(lesson1,0));
