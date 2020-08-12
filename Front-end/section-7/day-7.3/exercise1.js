36bd68236e0e4cdcc180396c15ad9be7ccf534bd
isabellavjs <isabellavjs@gmail.com>
2020-08-12 18:54:26 -0300

    Corrections exercise 1

--- Front-end/section-7/day-7.3/exercise1.js
+++ Front-end/section-7/day-7.3/exercise1.js
@@ -9,6 +9,13 @@ function sum(a, b) {
 }
 
 // implemente seus testes aqui - Tarefa 1: testar a funcao soma
+assert.strictEqual(typeof sum, 'function', 'sum nao e uma funcao')
 assert.strictEqual(sum(4,5),9, 'Quatro mais cinco e 9');
 assert.strictEqual(sum(0,0),0,'A soma 0 + 0 e 0');
-assert.strictEqual(sum(4,"5"),9,'A soma de quatro e cinco e nove');
+assert.throws(() => {
+  sum(4,'5');
+})
+//console.log(sum(4,'5'));
+assert.throws(() => {
+  sum(4,'5')
+}, /^Error: parameters must be numbers$/);
