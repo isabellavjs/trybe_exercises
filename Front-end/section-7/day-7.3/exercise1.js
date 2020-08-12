d6b8c221fbfbaf9fa161c0caa648a5ae1783f483
isabellavjs <isabellavjs@gmail.com>
2020-08-12 18:13:59 -0300

    Exercicio 1

--- Front-end/section-7/day-7.3/exercise1.js
+++ Front-end/section-7/day-7.3/exercise1.js
@@ -8,4 +8,7 @@ function sum(a, b) {
   return a + b;
 }
 
-// implemente seus testes aqui
+// implemente seus testes aqui - Tarefa 1: testar a funcao soma
+assert.strictEqual(sum(4,5),9, 'Quatro mais cinco e 9');
+assert.strictEqual(sum(0,0),0,'A soma 0 + 0 e 0');
+assert.strictEqual(sum(4,"5"),9,'A soma de quatro e cinco e nove');
