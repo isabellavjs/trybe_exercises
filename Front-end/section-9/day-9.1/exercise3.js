23a503f7fcd3faefc0e58a63a01b452ead4f76e3
isabellavjs <isabellavjs@gmail.com>
2020-08-26 20:10:07 -0300

    Exercises 3 and 4

---
+++ Front-end/section-9/day-9.1/exercise3.js
@@ -0,0 +1,18 @@
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+  //setTimeout(() => {
+    //console.log("Returned planet: ", mars);
+  //}, 4000); 
+};
+
+setTimeout(() => {
+  console.log(getPlanet());
+}, 4000);
+//getPlanet(); // imprime Marte depois de 4 segundos
