8810845968e046c54b57f3601e50560311ccb277
isabellavjs <isabellavjs@gmail.com>
2020-08-27 19:31:57 -0300

    Exercise 1

--- Front-end/section-9/day-9.2/apiScript.js
+++ Front-end/section-9/day-9.2/apiScript.js
@@ -1,7 +1,14 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
-  // Adicionar lógica aqui!
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
 };
+
+const fetchJoke = () => {
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => document.getElementById('jokeContainer').innerHTML = data.joke);
+}
 
 window.onload = () => fetchJoke();
